816b8ecba2d0c82fa2e57889242354742f656daf
EgorKhantakov <55573076+EgorKhantakov@users.noreply.github.com>
2020-10-02 00:53:57 +0700

    Update khantakov.py

--- khantakov.py
+++ khantakov.py
@@ -1,15 +1,17 @@
 a = 1
+b = 1
 nskarea = 892000000000
 #sm in square
 weight = 0.000000005
 capacity = 5*3*2
-for i in range(64):
-    a *=2
-allcapacity = capacity*(a-1)
+for i in range(63):
+    a = a * 2
+    b = b + a
+allcapacity = capacity*(b)
 height = allcapacity/nskarea
-allweight = (a-1)*weight
+allweight = (b)*weight
 time = allweight/2600000
-print("Зерен пшеницы:", a-1)
+print("Зерен пшеницы:", b)
 print("Масса зерна =", allweight,"тонн")
 print("Высота слоя для Советского района НСК =", height/100, "м")
 print("Время выращивания такого кол-ва зерна в НСК области =", time,"лет")
